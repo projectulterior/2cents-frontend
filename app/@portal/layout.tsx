355750b5a8ef13981854783646f5ce8d0cfc840b
Soung Bae Kim <sbkim@berkeley.edu>
2023-09-06 07:16:32 -0700

    profile

--- app/@portal/layout.tsx
+++ app/@portal/layout.tsx
@@ -35,7 +35,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                     style={{
                         flex: 3,
                         maxWidth: 1000,
-                        background: 'yellow',
+                        // background: 'yellow',
                     }}
                 >
                     {children}
--- app/@portal/profile/page.tsx
+++ app/@portal/profile/page.tsx
@@ -5,6 +5,7 @@ import Header from '../_components/Header';
 import { QUERY_GET_USER } from '@/gql/user';
 import { CoreUserFieldsFragment } from '@/gql/__generated__/graphql';
 import Loading from '@/components/Loading';
+import Image from 'next/image';
 
 export default function () {
     const { loading, data, error, fetchMore } = useQuery(QUERY_GET_USER, {
@@ -16,10 +17,12 @@ export default function () {
         return <Loading />;
     }
 
+    const user: any = data?.user;
+
     return (
         <>
-            <Header name={data?.user.name} />
-            <Profile user={data?.user as any} />
+            <Header name={user.name} />
+            <Profile user={user} />
         </>
     );
 }
@@ -27,8 +30,54 @@ export default function () {
 function Profile({ user }: { user: CoreUserFieldsFragment }) {
     return (
         <div style={{ background: 'pink' }}>
-            <h1>@{user.username}</h1>
-            <p>{user.bio}</p>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    maxHeight: '300px',
+                    height: '100vh',
+                    width: '100%',
+                    position: 'relative',
+                }}
+            >
+                <Image
+                    style={{
+                        objectFit: 'cover',
+                    }}
+                    fill={true}
+                    priority={true}
+                    src={user.cover ? user.cover : '/no-cover.png'}
+                    alt=""
+                />
+            </div>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    width: '30%',
+                    aspectRatio: 1 / 1,
+                    position: 'relative',
+                    borderRadius: '1000px',
+                    borderWidth: 5,
+                    overflow: 'hidden',
+                    top: -150,
+                    left: 75,
+                }}
+            >
+                <Image
+                    style={{
+                        objectFit: 'cover',
+                    }}
+                    fill={true}
+                    priority={true}
+                    src={user.profile ? user.profile : '/me.jpg'}
+                    alt=""
+                />
+            </div>
+            <p className="text-3xl font-semibold">@{user.username}</p>
+            <p className="" style={{ color: 'grey' }}>
+                {user.bio}
+            </p>
         </div>
     );
 }
